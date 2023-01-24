c02b89405d7183faded0791af1f7842f708a2b49
George Torres <george.torres.641@gmail.com>
2023-01-24 09:37:22 -0800

    first commit

---
+++ lab1.cpp
@@ -0,0 +1,16 @@
+//George Torres
+//Lab-1 Jan 24
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    int a = (rand() % 10) + 1; //RAND MAX
+    cout << a << endl;
+    cout << "Hello World\n";  
+    cout << "Second change\n";   
+
+    return 0;
+}
+
